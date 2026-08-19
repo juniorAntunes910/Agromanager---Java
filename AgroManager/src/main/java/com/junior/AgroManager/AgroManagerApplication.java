ed00419437f3d3375b93d706e8652b438bf79746
juniorAntunes910 <jugabriel2612@gmail.com>
2026-08-19 19:35:43 -0300

    feat(Project): Init project

---
+++ AgroManager/src/main/java/com/junior/AgroManager/AgroManagerApplication.java
@@ -0,0 +1,13 @@
+package com.junior.AgroManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroManagerApplication.class, args);
+	}
+
+}
---
+++ AgroManager/src/test/java/com/junior/AgroManager/AgroManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.AgroManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
