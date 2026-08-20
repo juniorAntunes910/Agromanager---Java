3712ab8c0632b15fc74dc6c7206d20bca98c5b5f
Junior Antunes <jugabriel2612@gmail.com>
2026-08-20 14:00:33 -0300

    feat(Enum): Create agroEventType, AlerStatus, AletSeverity, AuditAction, EmailType, EventType, NotificationCategory

---
+++ AgroManager (1)/AgroManager/src/main/java/com/junior/AgroManager/enums/AuditAction.java
@@ -0,0 +1,12 @@
+package com.junior.AgroManager.enums;
+
+public enum AuditAction {
+    CREATE,
+    UPDATE,
+    DELETE,
+    LOGIN,
+    LOGOUT,
+    APPROVE,
+    REJECT,
+    CANCEL
+}
---
+++ AgroManager (1)/AgroManager/src/main/java/com/junior/AgroManager/enums/EventType.java
@@ -0,0 +1,11 @@
+package com.junior.AgroManager.enums;
+
+public enum EventType {
+    CREATED,
+    UPDATED,
+    DELETED,
+    APPROVED,
+    REJECTED,
+    CANCELED,
+    COMPLETED
+}
